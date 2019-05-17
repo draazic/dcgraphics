1b5f2b2aef1df1956908f87d3d8bd654ecbd240e
carl <decarvalho.karl@gmail.com>
2019-05-17 09:24:33 +0200

    stagger

--- dcgraphicsApp/src/app/router.animations.ts
+++ dcgraphicsApp/src/app/router.animations.ts
@@ -2,24 +2,9 @@ import {trigger,style,transition,animate,keyframes,query,stagger} from '@angular
 
 
 
-// export const routerTransition = trigger('routerTransition',[
-//     transition('* => *', [
-
-//       query(':enter' , style({opacity: 0}),{optional: true}),
-
-//       query(':enter', stagger('300ms', [
-//         animate('1s ease-in', keyframes([
-//           style({opacity: 0, transform:'translateY(-75px)', offset: 0}),
-//           style({opacity: 0.5, transform:'translateY(35px)', offset: 0.3}),
-//           style({opacity: 1, transform:'translateY(0px)', offset: 1}),
-//         ]))
-//       ]))
-//     ])
-//   ])
-
 export const routerTransition = trigger('routerTransition', [
   transition('* <=> *', [
-    query(':enter, :leave', style({ position: 'fixed', width:'100%' })
+    query(':enter, :leave ', style({ position: 'fixed', width:'100%' })
       , { optional: true }),
     query('.block', style({ opacity: 0 })
       , { optional: true }),
