872887ed8bf447241ef3a3050ccaade030edf35c
deejaydraaz <deejaydraaz@MacBook-Pro-de-deejaydraaz.local>
2020-01-13 19:58:09 +0100

    tempFile

--- dcgraphicsApp/src/app/portfolio/portfolio.component.ts
+++ dcgraphicsApp/src/app/portfolio/portfolio.component.ts
@@ -1,11 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import {PortfolioService} from '../service/portfolio.service';
+import {trigger,style,transition,animate,keyframes,query,stagger} from '@angular/animations';
+
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
+  animations: [
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(-15px)' }),
+            stagger(
+              '50ms',
+              animate(
+                '550ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0px)' })
+              )
+            )
+          ],
+          { optional: true }
+        ),
+        query(':leave', animate('50ms', style({ opacity: 0 })), {
+          optional: true
+        })
+      ])
+    ])
+  ]
 })
+
+
 export class PortfolioComponent implements OnInit {
   private images =[];
   breakpoint: number;
@@ -16,8 +43,8 @@ export class PortfolioComponent implements OnInit {
   }
 
   ngOnInit() {
-    //this.breakpoint = (window.innerWidth >= 851) ? 4 : null;
-    //this.breakpoint = (window.innerWidth <= 850) ? 2 : 4;
+    //this.breakpoint = (window.innerWidth >= 1024) ? 8 : 8;
+    this.breakpoint = (window.innerWidth <= 850) ? 4 : 4;
     this.breakpoint = (window.innerWidth <= 400) ? 1 : 4;
    
     this.portfolioService.getPortfolios().subscribe((res : any[])=>{
@@ -25,13 +52,13 @@ export class PortfolioComponent implements OnInit {
       console.log(this.images);
 
       var array=this.images.length;
-      console.log(array);
+     
       for (var i = 0; i < array; i++){
 
         this.images[i].cols=Math.floor(Math.random() * 1)+1;
         this.images[i].rows=Math.floor(Math.random() * 4)+2;       
         this.images[i].color='#DDBDF1';
-        console.log(this.images);
+        
       }
     });
   
@@ -39,8 +66,8 @@ export class PortfolioComponent implements OnInit {
   }
   onResize(event) {
     console.log("coucou")
-    //this.breakpoint = (event.target.innerWidth >= 851) ? 4 : null;
-    //this.breakpoint = (event.target.innerWidth <= 850) ? 2 : 4;
+    //this.breakpoint = (window.innerWidth >= 1024) ? 4 : null;
+    this.breakpoint = (event.target.innerWidth <= 850) ? 4 : 4;
     this.breakpoint = (event.target.innerWidth <= 420) ? 1 : 4;
 
   }
